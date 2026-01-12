911b574b4159e0ae05414234b0bc4a127c25fa7d
ARSHSANDHU5254 <arshsandhu98550@gmail.com>
2026-01-13 00:21:43 +1100

    initial code for question 3

---
+++ src/Q3.py
@@ -0,0 +1,8 @@
+import turtle
+
+sides = int(input("Enter  number of sides: "))
+length = int(input("Enter side length: "))
+depth = int(input("Enter recursion depth: "))
+
+turtle.speed(0)
+turtle.hideturtle()

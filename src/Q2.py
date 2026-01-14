3fec2d841f983c75b8d64d89199a06f1d5dc4e96
harteet <harteetpalsingh2002@gmail.com>
2026-01-14 15:28:09 +1100

    function for reading all csv files created

--- src/Q2.py
+++ src/Q2.py
@@ -19,3 +19,44 @@ summer_months = ["December","January","February"]
 autumn_months = ["March","April","May"]
 winter_months = ["June","July","August"]
 spring_months = ["September","October","November"]
+
+# main function to read all csv files and process data
+
+for file_name in os.listdir(temperature_folder):
+    if file_name.endswith(".csv"):
+        file_path = os.path.join(temperature_folder, file_name)
+
+        with open(file_path, "r") as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                station = row["STATION_NAME"]
+
+                
+                if station not in station_temps:
+                    station_temps[station] = []
+
+            
+                for month in (summer_months + autumn_months +
+                              winter_months + spring_months):
+
+                    value = row[month]
+
+                
+                    if value == "" or value.lower() == "nan":
+                        continue
+
+                    temp = float(value)
+
+                
+                    station_temps[station].append(temp)
+
+                
+                    if month in summer_months:
+                        summer.append(temp)
+                    elif month in autumn_months:
+                        autumn.append(temp)
+                    elif month in winter_months:
+                        winter.append(temp)
+                    else:
+                        spring.append(temp)
